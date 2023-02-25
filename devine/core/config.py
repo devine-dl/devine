7958ad5e7cad34346f408bf26e582df4d962d30f
rlaphoenix <rlaphoenix@pm.me>
2023-02-25 23:00:50 +0000

    Create blank config if it did not exist

--- devine/core/config.py
+++ devine/core/config.py
@@ -74,9 +74,9 @@ class Config:
 
 
 # noinspection PyProtectedMember
-try:
-    config = Config.from_yaml(Config._Directories.user_configs / Config._Filenames.root_config)
-except FileNotFoundError:
-    config = Config()
+config_path = Config._Directories.user_configs / Config._Filenames.root_config
+if not config_path.is_file():
+    config_path.write_text("")
+config = Config.from_yaml(config_path)
 
 __ALL__ = (config,)
