95802d1e64f43883c57c5409a28c21f3aa28a52e
rlaphoenix <rlaphoenix@pm.me>
2023-05-12 12:19:34 +0100

    Fix regression with downloader mapper on aria2c and saldl
    The setup I had for using asyncio.run with functools.partial didn't actually pan out. A full pass-through lambda is required.
    
    I've also moved the mapped downloader variable to the root of the downloaders package.

--- devine/core/downloaders/__init__.py
+++ devine/core/downloaders/__init__.py
@@ -1,6 +1,15 @@
+import asyncio
+
 from .aria2c import aria2c
 from .requests import requests
 from .saldl import saldl
-from .downloader import downloader
+from ..config import config
+
+downloader = {
+    "aria2c": lambda *args, **kwargs: asyncio.run(aria2c(*args, **kwargs)),
+    "requests": requests,
+    "saldl": lambda *args, **kwargs: asyncio.run(saldl(*args, **kwargs))
+}[config.downloader]
+
 
 __ALL__ = (downloader, aria2c, requests, saldl)
