a01766c60bda9e1554cc024b191dae0dcb3dbe19
rlaphoenix <rlaphoenix@pm.me>
2023-05-31 23:04:48 +0100

    Remove the saldl downloader

--- devine/core/downloaders/__init__.py
+++ devine/core/downloaders/__init__.py
@@ -2,14 +2,12 @@ import asyncio
 
 from .aria2c import aria2c
 from .requests import requests
-from .saldl import saldl
 from ..config import config
 
 downloader = {
     "aria2c": lambda *args, **kwargs: asyncio.run(aria2c(*args, **kwargs)),
-    "requests": requests,
-    "saldl": lambda *args, **kwargs: asyncio.run(saldl(*args, **kwargs))
+    "requests": requests
 }[config.downloader]
 
 
-__ALL__ = (downloader, aria2c, requests, saldl)
+__ALL__ = (downloader, aria2c, requests)
