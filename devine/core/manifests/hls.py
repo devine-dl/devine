e26e55caf35be1aa419d30ca05d199c39727b3f4
rlaphoenix <rlaphoenix@pm.me>
2024-02-16 16:50:12 +0000

    HLS: Don't reset EXT-X-KEY state data on discontinuity

--- devine/core/manifests/hls.py
+++ devine/core/manifests/hls.py
@@ -412,7 +412,6 @@ class HLS:
                 discon_i += 1
                 range_offset = 0  # TODO: Should this be reset or not?
                 map_data = None
-                encryption_data = None  # TODO: Should this be reset or not?
 
             if segment.init_section and (not map_data or segment.init_section != map_data[0]):
                 if segment.init_section.byterange:
